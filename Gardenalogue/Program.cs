9f208ac7d3dc426028d5f31adad2910a9c643f4c
billy <matthewrdennis@gmail.com>
2019-09-15 16:12:01 +0100

    Rename

--- Gardenalogue/Program.cs
+++ Gardenalogue/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Plantalogue.Web
+namespace Gardenalogue.Web
 {
     public class Program
     {
