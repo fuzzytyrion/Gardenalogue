94e9127ab84b5135acc1505cab6bd30741a9c37e
fuzzytyrion <matthewrdennis@gmail.com>
2019-09-19 07:59:17 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,16 @@
 # Test
+function Exec {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
 echo "`n`n----- TEST -----`n"
 
 exec { & dotnet tool install --global coverlet.console }
