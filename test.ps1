39475334539c01a6628a2fc8df59b13d47eb6962
fuzzytyrion <matthewrdennis@gmail.com>
2019-09-19 08:08:21 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -15,17 +15,7 @@ echo "`n`n----- TEST -----`n"
 
 exec { & dotnet tool install --global coverlet.console }
 
-$testDirs  = @(Get-ChildItem -Path tests -Include "*.Test" -Directory -Recurse)
-
-$i = 0
-ForEach ($folder in $testDirs) { 
-    echo "Testing $folder"
-
-    $i++
-    $format = @{ $true = "/p:CoverletOutputFormat=opencover"; $false = ""}[$i -eq $testDirs.Length ]
-
-    exec { & dotnet test $folder.FullName -c Release --no-build --no-restore /p:CollectCoverage=true /p:CoverletOutput=$root\coverage /p:MergeWith=$root\coverage.json /p:Include="[*]Gardenalogue.*" /p:Exclude="[*]Gardenalogue.Testing.*" $format }
-}
+exec { & dotnet test src /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=$root\coverage /p:MergeWith=$root\coverage.json /p:Include="[*]Gardenalogue.*" /p:Exclude="[*]Gardenalogue.Testing.*" $format }
 
 choco install codecov --no-progress
 exec { & codecov -f "$root\coverage.opencover.xml" }
