395fbf0ee112a51564aeb299c3e8e4057d61fb4c
billy <matthewrdennis@gmail.com>
2019-10-22 20:50:43 +0100

    Add crud methods

--- src/Gardenalogue.Web/ClientApp/src/app/gardens/gardens.component.ts
+++ src/Gardenalogue.Web/ClientApp/src/app/gardens/gardens.component.ts
@@ -3,26 +3,35 @@ import { Garden } from '../models/garden';
 import { GardenService } from '../services/garden.service';
 
 @Component({
-  selector: 'app-gardens',
-  templateUrl: './gardens.component.html',
-  styleUrls: ['./gardens.component.css']
+    selector: 'app-gardens',
+    templateUrl: './gardens.component.html',
+    styleUrls: ['./gardens.component.css']
 })
 export class GardensComponent implements OnInit {
-  gardens: Garden[];
-  selectedGarden: Garden;
-  constructor(private gardenService: GardenService) { }
+    gardens: Garden[];
+    constructor(private gardenService: GardenService) { }
 
-  ngOnInit() {
-    this.getGardens();
-  }
+    ngOnInit() {
+        this.getGardens();
+    }
 
-  onSelect(garden: Garden): void {
-    this.selectedGarden = garden;
-  }
+    getGardens(): void {
+        this.gardenService.getGardens()
+            .subscribe(gardens => this.gardens = gardens);
+    }
 
-  getGardens(): void {
-    this.gardenService.getGardens()
-      .subscribe(gardens => this.gardens = gardens);
-  }
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.gardenService.addGarden({ name } as Garden)
+            .subscribe(result => {
+                this.getGardens();
+            });
+    }
 
+    delete(garden: Garden): void {
+        this.gardenService.deleteGarden(garden).subscribe(result => {
+            this.getGardens();
+        });
+    }
 }
